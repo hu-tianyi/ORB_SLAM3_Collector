f4dc4bdb8c1bf361d85f226533caf5b1bd9aa6f0
hu_tianyi <tianyi.hu@duke.edu>
2023-06-15 17:15:53 -0400

    Create the Data Collection module

---
+++ include/DataCollecting.h
@@ -0,0 +1,12 @@
+//
+// Created by tianyi on 6/15/23.
+//
+
+#ifndef ORB_SLAM3_DATACOLLECTING_H
+#define ORB_SLAM3_DATACOLLECTING_H
+
+#include "Tracking.h"
+#include "LocalMapping.h"
+#include "LoopClosing.h"
+
+#endif //ORB_SLAM3_DATACOLLECTING_H
---
+++ src/DataCollecting.cpp
@@ -0,0 +1,9 @@
+//
+// Created by tianyi on 6/15/23.
+//
+
+#include "DataCollecting.h"
+
+#include "Tracking.h"
+#include "LocalMapping.h"
+#include "LoopClosing.h"
