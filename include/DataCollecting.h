8f7cac9e8fdeff735a7f7ff66baadad0760166ae
hu_tianyi <tianyi.hu@duke.edu>
2023-06-15 23:29:53 -0400

    Add DataCollecting Module's source code and header file

--- include/DataCollecting.h
+++ include/DataCollecting.h
@@ -9,4 +9,41 @@
 #include "LocalMapping.h"
 #include "LoopClosing.h"
 
+namespace  ORB_SLAM3
+{
+class System;
+class Tracking;
+class LoopClosing;
+
+class DataCollecting
+{
+public:
+    DataCollecting(System* pSys, Atlas* pAtlas, const float bMonocular, bool bInertial, const string &_strSeqName=std::string());
+
+    void SetTracker(Tracking* pTracker);
+
+    void SetLocalMapper(LocalMapping *pLocalMapper);
+
+    void SetLoopCloser(LoopClosing* pLoopCloser);
+
+    void InitializeCSVLogger();
+
+protected:
+
+    System *mpSystem;
+    Atlas* mpAtlas;
+    bool mbMonocular;
+    bool mbInertial;
+
+    // member pointers to the three main modules
+    Tracking* mpTracker;
+    LocalMapping mpLocalMapper;
+    LoopClosing* mpLoopCloser;
+
+};
+
+
+}
+
+
 #endif //ORB_SLAM3_DATACOLLECTING_H
--- src/DataCollecting.cpp
+++ src/DataCollecting.cpp
@@ -7,3 +7,48 @@
 #include "Tracking.h"
 #include "LocalMapping.h"
 #include "LoopClosing.h"
+
+namespace ORB_SLAM3
+{
+
+DataCollecting::DataCollecting(System* pSys, Atlas *pAtlas, const float bMonocular, bool bInertial, const string &_strSeqName):
+        mpSystem(pSys), mbMonocular(bMonocular), mbInertial(bInertial)
+{
+
+}
+
+void DataCollecting::SetLoopCloser(LoopClosing* pLoopCloser)
+{
+    mpLoopCloser = pLoopCloser;
+}
+
+void DataCollecting::SetTracker(Tracking *pTracker)
+{
+    mpTracker=pTracker;
+}
+
+void DataCollecting::SetLocalMapper(LocalMapping *pLocalMapper)
+{
+    mpLocalMapper=pLocalMapper;
+}
+
+
+void DataCollecting::InitializeCSVLogger()
+{
+    // TODO
+}
+
+void DataCollecting::Run()
+{
+    mbFinished = false;
+
+    while(1)
+    {
+
+    }
+
+}
+
+
+
+}
