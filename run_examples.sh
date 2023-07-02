260b4058f50dce10491b8eb54f18e2b48da51ea3
hu_tianyi <tianyi.hu@duke.edu>
2023-07-02 15:36:27 -0400

    Update run_examples.sh

--- run_examples.sh
+++ run_examples.sh
@@ -1,1 +1,1 @@
-./Examples/Monocular/mono_euroc ./Vocabulary/ORBvoc.txt ./Examples/Monocular/EuRoC.yaml ./datasets/MH01 ./Examples/Monocular/EuRoC_TimeStamps/MH01.txt
+./Examples/Monocular/mono_euroc ./Vocabulary/ORBvoc.txt ./Examples/Monocular/EuRoC.yaml ./datasets/MH03 ./Examples/Monocular/EuRoC_TimeStamps/MH03.txt
