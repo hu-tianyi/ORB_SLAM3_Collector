0fd7898158b12e9dfdfad27900bc02a8038bf30c
hu_tianyi <tianyi.hu@duke.edu>
2023-07-06 21:12:48 -0400

    Add ZJU-SenseTime Dataset

--- run_examples.sh
+++ run_examples.sh
@@ -1,1 +1,5 @@
-./Examples/Monocular/mono_euroc ./Vocabulary/ORBvoc.txt ./Examples/Monocular/EuRoC.yaml ./datasets/MH03 ./Examples/Monocular/EuRoC_TimeStamps/MH03.txt
+# ./Examples/Monocular/mono_euroc ./Vocabulary/ORBvoc.txt ./Examples/Monocular/EuRoC.yaml ./datasets/MH03 ./Examples/Monocular/EuRoC_TimeStamps/MH03.txt
+
+./Examples/Monocular/mono_tum_vi ./Vocabulary/ORBvoc.txt ./Examples/Monocular/SenseTime.yaml ./datasets/A1/camera/images/ ./Examples/Monocular/SenseTime_TimeStamps/A1.txt
+
+# ./Examples/Monocular/mono_tum_vi ./Vocabulary/ORBvoc.txt ./Examples/Monocular/TUM-VI.yaml ./datasets/dataset-corridor4_512_16/mav0/cam0/data ./Examples/Monocular/TUM_TimeStamps/dataset-corridor4_512.txt
