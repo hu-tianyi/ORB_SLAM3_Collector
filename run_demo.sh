2f3cce207b5aae973f9430e3845d9f44494b7a6e
hu_tianyi <tianyi.hu@duke.edu>
2023-07-31 15:35:47 -0400

    update testing script

---
+++ run_demo.sh
@@ -0,0 +1,7 @@
+./Examples/Monocular/mono_tum_vi ./Vocabulary/ORBvoc.txt ./Examples/Monocular/TUM1D.yaml ./datasets/nasaGood/rgb/ ./Examples/Monocular/TUM1D_TimeStamps/nasaGood.txt #&
+
+
+sleep 5
+
+python ../DemoAnimation/demo2.py
+
--- run_examples.sh
+++ run_examples.sh
@@ -1,14 +1,14 @@
 #./Examples/Monocular/mono_euroc ./Vocabulary/ORBvoc.txt ./Examples/Monocular/EuRoC.yaml ./datasets/MH04 ./Examples/Monocular/EuRoC_TimeStamps/MH04.txt
 
-<<<<<<< Updated upstream
-./Examples/Monocular/mono_tum_vi ./Vocabulary/ORBvoc.txt ./Examples/Monocular/SenseTime.yaml ./datasets/A0/camera/images/ ./Examples/Monocular/SenseTime_TimeStamps/A0.txt
-=======
+#./Examples/Monocular/mono_tum_vi ./Vocabulary/ORBvoc.txt ./Examples/Monocular/SenseTime.yaml ./datasets/A0/camera/images/ ./Examples/Monocular/SenseTime_TimeStamps/A0.txt
 #./Examples/Monocular/mono_tum_vi ./Vocabulary/ORBvoc.txt ./Examples/Monocular/SenseTime.yaml ./datasets/A2/camera/images/ ./Examples/Monocular/SenseTime_TimeStamps/A2.txt
 
-#./Examples/Monocular/mono_tum_vi ./Vocabulary/ORBvoc.txt ./Examples/Monocular/SenseTime.yaml ./datasets/A3/camera/images/ ./Examples/Monocular/SenseTime_TimeStamps/A3.txt
->>>>>>> Stashed changes
+
+./Examples/Monocular/mono_tum_vi ./Vocabulary/ORBvoc.txt ./Examples/Monocular/SenseTime.yaml ./datasets/SenseTime/A1/camera/images/ ./Examples/Monocular/SenseTime_TimeStamps/A1.txt
+
+# ./Examples/Monocular/mono_tum_vi ./Vocabulary/ORBvoc.txt ./Examples/Monocular/SenseTime.yaml ./datasets/A3/camera/images/ ./Examples/Monocular/SenseTime_TimeStamps/A3.txt
 
 # ./Examples/Monocular/mono_tum_vi ./Vocabulary/ORBvoc.txt ./Examples/Monocular/TUM-VI.yaml ./datasets/dataset-corridor4_512_16/mav0/cam0/data ./Examples/Monocular/TUM_TimeStamps/dataset-corridor4_512.txt
 
-./Examples/Monocular/mono_tum_vi ./Vocabulary/ORBvoc.txt ./Examples/Monocular/TUM1D.yaml ./datasets/nasaGood/rgb/ ./Examples/Monocular/TUM1D_TimeStamps/nasaGood.txt
+#./Examples/Monocular/mono_tum_vi ./Vocabulary/ORBvoc.txt ./Examples/Monocular/TUM1D.yaml ./datasets/nasaGood/rgb/ ./Examples/Monocular/TUM1D_TimeStamps/nasaGood.txt
 
