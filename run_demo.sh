fe4998ccd8a69825cb018f69670dbd1db8c4484e
hu_tianyi <tianyi.hu@duke.edu>
2023-08-02 17:07:36 -0400

    Add data generator

--- run_demo.sh
+++ run_demo.sh
@@ -1,7 +1,16 @@
-./Examples/Monocular/mono_tum_vi ./Vocabulary/ORBvoc.txt ./Examples/Monocular/TUM1D.yaml ./datasets/nasaGood/rgb/ ./Examples/Monocular/TUM1D_TimeStamps/nasaGood.txt #&
+#./Examples/Monocular/mono_tum_vi ./Vocabulary/ORBvoc.txt ./Examples/Monocular/TUM1D.yaml ./datasets/nasaGood/rgb/ ./Examples/Monocular/TUM1D_TimeStamps/nasaGood.txt #&
+
+#./Examples/Monocular/mono_tum ./Vocabulary/ORBvoc.txt ./Examples/Monocular/TUM1.yaml ./datasets/TUM-RGBD/rgbd_dataset_freiburg1_room/rgbd_dataset_freiburg1_room/
+
+#./Examples/Monocular/mono_tum ./Vocabulary/ORBvoc.txt ./Examples/Monocular/TUM2.yaml ./datasets/TUM-RGBD/rgbd_dataset_freiburg2_large_no_loop/rgbd_dataset_freiburg2_large_no_loop/
+
+
+#./Examples/Monocular/mono_tum ./Vocabulary/ORBvoc.txt ./Examples/Monocular/TUM2.yaml ./datasets/TUM-RGBD/rgbd_dataset_freiburg2_large_with_loop/rgbd_dataset_freiburg2_large_with_loop/
+
+./Examples/Monocular/mono_tum ./Vocabulary/ORBvoc.txt ./Examples/Monocular/TUM2.yaml ./datasets/TUM-RGBD/rgbd_dataset_freiburg2_desk_with_person/rgbd_dataset_freiburg2_desk_with_person/
 
 
 sleep 5
 
-python ../DemoAnimation/demo2.py
+# python ../DemoAnimation/demo2.py
 
